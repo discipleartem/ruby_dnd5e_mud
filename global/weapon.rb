d363b210954909794b9659f4b89ff6b2ebf16507
Tomas <artemjob@ukr.net>
2020-08-08 02:34:56 +0300

    replace class Armor with armor.yml (data)

--- global/weapon.rb
+++ global/weapon.rb
@@ -6,7 +6,7 @@ class Weapon
   include Dice
 
   #melee, range
-  attr_accessor :type
+  attr_accessor :range
   #one or two_handed
   attr_accessor :handed
 
@@ -15,62 +15,69 @@ class Weapon
   attr_accessor :price
   attr_accessor :weight
   attr_accessor :name
+  attr_accessor :type
   attr_accessor :disadvantage
 
 
   def initialize
     @damage = 1
     @handed = ['one', 'two', 'universal']
-    @type = ['melee', 'range']
+    @range = ['melee', 'range']
+    @type = ['warrior_melee', 'simple_melee', 'warrior_range', 'simple_range', 'improvise']
   end
 
 
 
-  class Sword  < Weapon
+  class Sword < Weapon
+  attr_reader :type
+  attr_reader :name
+  attr_reader :range
+  attr_accessor :damage_type
 
 
-    class LongSword < Sword
-      def initialize
-        @name = 'long_sword'
-        @price = {gold: 15}
-        @weight = 3
-        @type = @type[0]
-        @handed = @handed[2]
-        @damage = [d8, d10]
-        @damage_type = ['рубящий']
-      end
+  def initialize
+    @name = ['long_sword']
+    @range = @range[0]
+    @damage_type = ['рубящий', 'колющий']
+  end
+
+  class LongSword < Sword
+    def initialize
+      @name = @name[0]
+      @price = {gold: 15}
+      @weight = 3
+      @handed = @handed[2]
+      @damage = [d8, d10]
+      @damage_type = @damage_type[0]
+      @type = @type[0]
     end
-
-
-
   end
 
-
-
+end
 
   class Bow <Weapon
 
-
-    def initialize
-      @need_ammo = true
-    end
-
-    class LongBow
-      def initialize(player)
-        @name = 'long_bow'
-        @price = {gold: 50}
-        @weight = 2
-        @type = @type[1]
-        @handed = @handed[1]
-        @damage = [d8]
-        @damage_type = ['колющий']
-        @disadvantage = player.size == 'small' ? true : false
-        @bow_distance = {min: 150, max: 600}
-      end
-    end
-
-
+  def initialize
+    @need_ammo = true
   end
+
+  class LongBow < Bow
+    def initialize(player)
+      @name = 'long_bow'
+      @type = @type[2]
+      @price = {gold: 50}
+      @weight = 2
+      @range = @range[1]
+      @handed = @handed[1]
+      @damage = [d8]
+      @damage_type = ['колющий']
+      @disadvantage = player.size == 'small' ? true : false
+      @bow_distance = {min: 150, max: 600}
+    end
+  end
+
+end
+
 
 
 
